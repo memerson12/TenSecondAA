34a43a369f03db20f0a8ea0a7fcb84b57c972f4e
Michael <47831816+memerson12@users.noreply.github.com>
2023-08-03 01:09:31 -0500

    Small refactor and countdown

--- src/main/java/com/memerson/tenSecondAA/TenSecondAA.java
+++ src/main/java/com/memerson/tenSecondAA/TenSecondAA.java
@@ -1,9 +1,9 @@
 package com.memerson.tenSecondAA;
 
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
+import net.minecraft.text.Text;
 import net.minecraft.world.GameMode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +14,7 @@ public class TenSecondAA implements ModInitializer {
     private static final int DELAY_TIME_SECS = 5;
     public static final Logger LOGGER = LogManager.getLogger("tsaa");
     private static ServerPlayerEntity player;
+    private static long previousTicksInWorld = -1;
 
     @Override
     public void onInitialize() {
@@ -24,10 +25,6 @@ public class TenSecondAA implements ModInitializer {
     public static void onJoin(ServerPlayerEntity player) {
         TenSecondAA.player = player;
         LOGGER.info("Player Joined!");
-        long ticksInWorld = player.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_ONE_MINUTE));
-        if(ticksInWorld > DELAY_TIME_SECS * 20 && ticksInWorld < CREATIVE_TIME_SECS * 20 + DELAY_TIME_SECS * 20) {
-            player.setGameMode(GameMode.CREATIVE);
-        }
     }
 
     public static void onTick() {
@@ -36,12 +33,24 @@ public class TenSecondAA implements ModInitializer {
             return;
         }
         long ticksInWorld = player.getStatHandler().getStat(Stats.CUSTOM.getOrCreateStat(Stats.PLAY_ONE_MINUTE));
-        if(ticksInWorld >= CREATIVE_TIME_SECS * 20 + DELAY_TIME_SECS * 20 && !player.interactionManager.isSurvivalLike()) {
-            player.setGameMode(GameMode.SURVIVAL);
-        } else if(ticksInWorld < CREATIVE_TIME_SECS * 20 + DELAY_TIME_SECS * 20) {
-            // Stop the player from being able to move
+//        LOGGER.info("Ticks in world: " + ticksInWorld);
+        if(ticksInWorld < DELAY_TIME_SECS * 20) {
             player.teleport(player.prevX, player.prevY, player.prevZ);
-
+            if(ticksInWorld % 20 == 0 && ticksInWorld != previousTicksInWorld) {
+                player.sendMessage(Text.method_30163((DELAY_TIME_SECS - (ticksInWorld / 20)) + " seconds until creative"), false);
+            }
+        } else if(ticksInWorld < CREATIVE_TIME_SECS * 20 + DELAY_TIME_SECS * 20) {
+            player.teleport(player.prevX, player.prevY, player.prevZ);
+            if(player.interactionManager.isSurvivalLike()) {
+                player.setGameMode(GameMode.CREATIVE);
+            }
+            if(ticksInWorld % 20 == 0 && ticksInWorld != previousTicksInWorld) {
+                player.sendMessage(Text.method_30163((CREATIVE_TIME_SECS - ((ticksInWorld - DELAY_TIME_SECS * 20) / 20)) + " seconds until survival"), false);
+            }
+        } else if(!player.interactionManager.isSurvivalLike()) {
+            player.setGameMode(GameMode.SURVIVAL);
+            player.sendMessage(Text.method_30163("You are now in survival mode"), false);
         }
+        previousTicksInWorld = ticksInWorld;
     }
 }
--- src/main/java/com/memerson/tenSecondAA/mixin/onJoinMixin.java
+++ src/main/java/com/memerson/tenSecondAA/mixin/onJoinMixin.java
@@ -1,28 +1,19 @@
 package com.memerson.tenSecondAA.mixin;
 
 import com.memerson.tenSecondAA.TenSecondAA;
-import net.minecraft.advancement.Advancement;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.stat.StatType;
-import net.minecraft.stat.Stats;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.BooleanSupplier;
 
 @Mixin(PlayerManager.class)
 public class onJoinMixin {
     @Inject(at = @At("TAIL"), method = "onPlayerConnect")
     private void onJoin(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
-        TenSecondAA.onJoin(player)
+        TenSecondAA.onJoin(player);
     }
 
 }
